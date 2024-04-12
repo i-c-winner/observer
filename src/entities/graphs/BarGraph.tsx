848884dbe4323a59abcb49c7d3c2f21680c80a20
Dmitriy Belousov <d-belousov@hotmail.com>
2024-04-12 18:27:00 +0500

    feat: Добавит данные

--- src/entities/graphs/BarGraph.tsx
+++ src/entities/graphs/BarGraph.tsx
@@ -1,19 +1,37 @@
 
 import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
 import {TBarGraphData} from "../types";
+// @ts-ignore
+import randomColor from 'randomcolor'
+
 interface IProps {
   data: TBarGraphData,
+  keys:string[]
 }
 
 function BarGraph (props: IProps) {
-  return <BarChart width={700} height={250} data={props.data}>
+  function getColor() {
+    return randomColor()
+  }
+console.log(props.data)
+  function getWidth() {
+    return  window.innerWidth
+  }
+  return <BarChart width={getWidth()} height={250} data={props.data}>
   <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="name" />
+  <XAxis dataKey="Day" />
   <YAxis />
   <Tooltip />
   <Legend />
- <Bar barSize={10} dataKey='alfa' fill={'red'} />
- <Bar barSize={10} dataKey='beta' fill={'green'} />
+    {props.keys.map((value, index)=>{
+      console.log(value)
+      if (value!=='Day'){
+        return <Bar
+          key={index}
+          barSize={2} dataKey={value} fill={getColor()}/>
+      }
+
+    })}
     </BarChart>
 }
 export {BarGraph}
--- src/pages/graphs/OEE.tsx
+++ src/pages/graphs/OEE.tsx
@@ -2,6 +2,35 @@ import { Box, Grid, Typography } from "@mui/material";
 import { BarGraph } from "../../entities/graphs/BarGraph";
 import { BarHeader } from "../../widgets/headers";
 import { useAuth } from "../../app/context/auth-context";
+import februaryJSON from '../../shared/assets/data/february.json'
+import januaryJSON from '../../shared/assets/data/january.json'
+import marchJSON from '../../shared/assets/data/march.json'
+import monthlyJSON from '../../shared/assets/data/monthly.json'
+
+
+
+// const february= JSON.parse(februaryJSON)
+// const january=JSON.parse(januaryJSON)
+// const march=JSON.parse(marchJSON)
+// const monthly=JSON.parse(monthlyJSON)
+
+
+
+function floorData(data: any) {
+  data.forEach((day: any) => {
+    for (const key in day) {
+      if (key !== 'Day') {
+        day[key] = Math.floor(day[key])
+      }
+    }
+  })
+}
+floorData(januaryJSON)
+floorData(februaryJSON)
+floorData(marchJSON)
+
+
+console.log(januaryJSON)
 
 const data = [
   {
@@ -16,31 +45,22 @@ const data = [
   }
 ]
 
+// const keys=Object.keys(data[0])
+const keys = Object.keys(januaryJSON[0])
+
+
 function OEE() {
   const {user} = useAuth()
   if (user) {
     return <Box>
       <BarHeader name="OEE" titles={['Годовой обзор по неделям', 'Годовой обзор по месяцам']}/>
-      <Grid container spacing={2} p={5}>
-        <Grid item xs={6}>
-          <BarGraph data={data}/>
-        </Grid>
-        <Grid item xs={6}>
-          <BarGraph data={data}/>
-        </Grid>
-        <Grid item xs={6}>
-          <BarGraph data={data}/>
-        </Grid>
-        <Grid item xs={6}>
-          <BarGraph data={data}/>
-        </Grid>
-        <Grid item xs={6}>
-          <BarGraph data={data}/>
-        </Grid>
-        <Grid item xs={6}>
-          <BarGraph data={data}/>
-        </Grid>
-      </Grid>
+      <h2>Январь</h2>
+      <BarGraph keys={keys} data={januaryJSON}/>
+      <h2>Февраль</h2>
+      <BarGraph keys={keys} data={februaryJSON}/>
+      <h3>Март</h3>
+      <BarGraph keys={keys} data={marchJSON}/>
+
     </Box>
   } else {
     return <Typography>не аутентифицирован</Typography>
